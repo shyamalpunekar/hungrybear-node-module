fabb2e662a860eb637e64f78f20da9388feee0dc
Kira Loo and Shyamal Punekar <shyamal@>
2017-09-11 14:37:07 -0700

    Add closures

--- js/hungrybear.js
+++ js/hungrybear.js
@@ -1,26 +1,63 @@
-export class hungryBear {
-
-  constructor(name) {
-    this.name = name;
-    this.foodLevel = 10;
-  }
-
-  setHunger() {
-    setInterval(() => {
+export let bear = {
+  foodLevel: 10,
+  setHunger: function() {
+    const hungerInterval = setInterval(() => {
       this.foodLevel--;
+      if (this.didYouGetEaten() == true) {
+        clearInterval(hungerInterval);
+        return "You got eaten1";
+      }
     }, 1000);
-  }
-
-  didYouGetEaten() {
-    if (this.foodLevel > 0) {
+  },
+  didYouGetEaten: function() {
+    if (this.foodLevel >0) {
       return false;
     } else {
       return true;
     }
+  },
+  feed: function(amount) {
+    let that = this;
+    return function(food){
+      that.foodLevel += amount
+      return `The bear ate the ${food}! Food level goes up ${amount}!`
+    }
   }
+};
 
-  feed() {
-    this.foodLevel = 10;
-  }  
+bear.eatSmall = bear.feed(5);
+bear.eatMedium = bear.feed(10);
+bear.eatLarge = bear.feed(15);
+bear.eatYuck = bear.feed(-10);
+bear.eatPowerUp = bear.feed(50);
+bear.eatSpecialBonus = bear.feed(100);
+bear.eatWeirdThing = bear.feed(Math.floor((Math.random() * 20) + 1));
 
-}
+
+
+// class hungryBear {
+//
+//   constructor(name) {
+//     this.name = name;
+//     this.foodLevel = 10;
+//   }
+//
+//   setHunger() {
+//     setInterval(() => {
+//       this.foodLevel--;
+//     }, 1000);
+//   }
+//
+//   didYouGetEaten() {
+//     if (this.foodLevel > 0) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+//
+//   feed() {
+//     this.foodLevel = 10;
+//   }
+//
+// }
--- spec/hungry-bear-spec.js
+++ spec/hungry-bear-spec.js
@@ -1,39 +1,47 @@
-import { hungryBear } from './../js/hungrybear.js';
+import { bear } from './../js/hungrybear.js';
 
 describe('hungryBear', function() {
-  let fuzzy = new hungryBear("Fuzzy");
+  let fuzzy = bear;
 
   beforeEach(function() {
     jasmine.clock().install();
+    fuzzy.foodLevel = 10;
+    fuzzy.name = "Fuzzy";
     fuzzy.setHunger();
   });
+
   afterEach(function() {
     jasmine.clock().uninstall();
   });
 
-  it('should have a name and a food level of 10 when it is created', function() {
-    expect(fuzzy.name).toEqual("Fuzzy");
-    expect(fuzzy.foodLevel).toEqual(10);
+  it('should return that the bear ate blueberries and the food level should go up 5', function() {
+    expect(fuzzy.eatSmall("blueberries")).toEqual("The bear ate the blueberries! Food level goes up 5!");
+    expect(fuzzy.foodLevel).toEqual(15);
   });
 
-  it('should have a food level of 7 after 3001 milliseconds', function() {
-    jasmine.clock().tick(3001);
-    expect(fuzzy.foodLevel).toEqual(7);
-  });
-
-  it('should get very hungry if the food level drops below zero', function() {
-    fuzzy.foodLevel = 0;
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
-
-  it('should get very hungry if 10 seconds pass without feeding', function() {
-    jasmine.clock().tick(10001);
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
-
-  it('should have a food level of ten if it is fed', function() {
-    jasmine.clock().tick(9001);
-    fuzzy.feed();
-    expect(fuzzy.foodLevel).toEqual(10);
-  });
+  // it('should have a name and a food level of 10 when it is created', function() {
+  //   expect(fuzzy.name).toEqual("Fuzzy");
+  //   expect(fuzzy.foodLevel).toEqual(10);
+  // });
+  //
+  // it('should have a food level of 7 after 3001 milliseconds', function() {
+  //   jasmine.clock().tick(3001);
+  //   expect(fuzzy.foodLevel).toEqual(7);
+  // });
+  //
+  // it('should get very hungry if the food level drops below zero', function() {
+  //   fuzzy.foodLevel = 0;
+  //   expect(fuzzy.didYouGetEaten()).toEqual(true);
+  // });
+  //
+  // it('should get very hungry if 10 seconds pass without feeding', function() {
+  //   jasmine.clock().tick(10001);
+  //   expect(fuzzy.didYouGetEaten()).toEqual(true);
+  // });
+  //
+  // it('should have a food level of ten if it is fed', function() {
+  //   jasmine.clock().tick(9001);
+  //   fuzzy.feed();
+  //   expect(fuzzy.foodLevel).toEqual(10);
+  
 });
